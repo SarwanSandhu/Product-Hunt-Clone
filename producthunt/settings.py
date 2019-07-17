909860e054efefc762b185aa1bf2ba40a68e73a8
SarwanSandhu <sandhusarwan1@gmail.com>
2019-07-17 16:39:39 -0400

    design home page

--- producthunt/settings.py
+++ producthunt/settings.py
@@ -126,5 +126,8 @@ STATICFILES_DIRS=[
 os.path.join(BASE_DIR, 'producthunt/static/')
 ]
 
-STATIC_ROOT= os.path.join(BASE_DIR, 'static')
+MEDIA_ROOT= os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
